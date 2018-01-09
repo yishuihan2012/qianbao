5f491c29ab829cdf4703f9317d1586cbdfde173a
xuchengcheng <1374687780@qq.com>
2018-01-10 00:32:17 +0800

    call_android

--- template/Userurl/register.blade.php
+++ template/Userurl/register.blade.php
@@ -46,7 +46,7 @@
 			</form>
 			<div class="space-up">
 				<p><a class="my-btn-blue4" id="regBtn">注册</a></p>
-				<p class="space-up"><a class="my-btn-blue4" id="#download_app" download_url="/api/userurl/download">下载APP</a></p>
+				<p class="space-up"><a class="my-btn-blue4" id="#download_app" href="/api/userurl/download">下载APP</a></p>
 			</div>
 		</div>
 		<script src="/static/js/mui.min.js"></script>
@@ -57,7 +57,7 @@
 			mui.ready(function(){
 				//下载APP
 				$("#download_app").click(function(){
-					var	url=$this.attr('download_url');
+					var	url=$this.attr('href');
 					window.location.href=url;
 				})
 				//点击注册
@@ -84,7 +84,7 @@
 		            $.post(url,data,function(data){
 		              if(data.code==200){
 		              	mui.toast(data.msg); 
-		              	var	url=$('#download_app').attr('download_url');
+		              	var	url=$('#download_app').attr('href');
 		              	window.location.href=url;
 		              }else{
 		              	mui.toast("注册失败"); 
