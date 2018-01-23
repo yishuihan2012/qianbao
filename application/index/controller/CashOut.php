c3bae991d18dbf45c30e82e23405e61abfaf84ff
928791694@qq.com <928791694@qq.com>
2018-01-23 18:04:37 +0800

    gao

--- application/index/controller/CashOut.php
+++ application/index/controller/CashOut.php
@@ -490,7 +490,7 @@ class CashOut
 	 {
 	 	$item_rate=$this->also->item_rate/100;
 	 	$item_charges=$this->also->item_charges;
-	 	$url= System::getName('system_url').'/api/Userurl/H5youjifen/tradeNo'.$tradeNo;
+	 	$url= System::getName('system_url').'/api/Userurl/H5youjifen/tradeNo/'.$tradeNo;
 	 	 $arr= $price."|".$this->card_info->card_name."|".$this->card_info->card_idcard."|".$this->member_card->card_bankno."|".$this->card_info->card_phone."|".$this->card_info->card_bankname."|".$this->card_info->card_bankno."|".$this->card_info->card_phone."|".$this->card_info->card_bankname."| |".$url."|".$tradeNo."|".$item_rate."|".$item_charges;
 	 	 // echo $arr;die;
 	 	 $params['data']=H5encrypt($arr,$this->passway_info->passageway_key);
@@ -534,7 +534,7 @@ class CashOut
 	 {
 	 	$item_rate=$this->also->item_rate/100;
 	 	$item_charges=$this->also->item_charges;
-	 	$url= System::getName('system_url').'/api/Userurl/H5youjifen/tradeNo'.$tradeNo;
+	 	$url= System::getName('system_url').'/api/Userurl/H5youjifen/tradeNo/'.$tradeNo;
 	 	 $arr= $price."|".$this->card_info->card_name."|".$this->card_info->card_idcard."|".$this->member_card->card_bankno."|".$this->card_info->card_phone."|".$this->card_info->card_bankno."|".$this->card_info->card_phone."| |".$url."|".$tradeNo."|".$item_rate."|".$item_charges;
 	 	 // echo $arr;die;
 	 	 $params['data']=H5encrypt($arr,$this->passway_info->passageway_key);
@@ -575,7 +575,7 @@ class CashOut
 	 {
 	 	$item_rate=$this->also->item_rate/100;
 	 	$item_charges=$this->also->item_charges;
-	 	$url= System::getName('system_url').'/api/Userurl/H5youjifen/tradeNo'.$tradeNo;
+	 	$url= System::getName('system_url').'/api/Userurl/H5youjifen/tradeNo/'.$tradeNo;
 	 	 $arr= $price."|".$this->card_info->card_name."|".$this->card_info->card_idcard."|".$this->member_card->card_bankno."|".$this->card_info->card_phone."|".$this->card_info->card_bankno."|".$this->card_info->card_phone."| |".$url."|".$tradeNo."|".$item_rate."|".$item_charges;
 	 	 // echo $arr;die;
 	 	 $params['data']=H5encrypt($arr,$this->passway_info->passageway_key);
