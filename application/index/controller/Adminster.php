dedd06b1a789e1a3bddc336312f1aa440c106a58
yangde1990 <2550607202@qq.com>
2018-01-24 13:13:01 +0800

    -

--- application/index/controller/Adminster.php
+++ application/index/controller/Adminster.php
@@ -184,13 +184,13 @@ class Adminster extends Common {
 			 $users=db('member')->alias('m')
 			 	->join('member_group g','m.member_group_id=g.group_id')
 			 	->where('m.member_id','in',$this->admin['children'])
-	  		 	// ->where('g.group_visible=0')
+	  		 	// ->where('g.group_visible=0')#没有该字段
 			 	->select();
 		 }else{
 			 $authGroups=AuthGroups::all();
 			 $users=db('member')->alias('m')
 			 	->join('member_group g','m.member_group_id=g.group_id')
-			 	// ->where('g.group_visible=0')
+			 	// ->where('g.group_visible=0')#没有该字段
 			 	->select();
 		 }
 		 $this->assign('users',$users);
