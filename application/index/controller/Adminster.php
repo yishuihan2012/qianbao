5ff2cb6461d02cdf83c8cdaf85abe797bda8eba6
star <star@qq.com>
2018-01-24 18:49:08 +0800

    修复添加代理用户BUG

--- application/index/controller/Adminster.php
+++ application/index/controller/Adminster.php
@@ -50,18 +50,11 @@ class Adminster extends Common {
 		 if(Request::instance()->isPost()){
 		 	$r=request()->param();
 			 $code=make_rand_code();
-			 
+			 $adminster=new Adminsters;
 			 #运营商添加的用户强制为 运营商用户组
 			 if($this->admin['adminster_group_id']==5){
 			 	$r['login_group']=5;
 			 }
-
-			 $check_login_email=Adminsters::get(['adminster_email'=>Request::instance()->post('login_email')]);
-			 if($check_login_email){
-			     	 Session::set('jump_msg',['type'=>'warning','msg'=>'邮箱已绑定其他账号~请更换','data'=>'']);
-				 $this->redirect($this->history['0']);
-			 }
-			 $adminster=new Adminsters;
 			 if($r['login_group']==5){
 			 	if($r['adminster_user_id']){
 			 		$admin=Adminsters::get(['adminster_user_id'=>$r['adminster_user_id']]);
@@ -77,18 +70,24 @@ class Adminster extends Common {
 			 	}
 			 	#新增的代理商后台账户 其用户名自动调整为对应后台手机号
 			 	$member=db('member')->where('member_id',$r['adminster_user_id'])->find();
-			 	$r['login_name']=$member['member_mobile'];
+			 	$r['adminster_login']=$member['member_mobile'];
+			 }else{
+				 $check_login_email=Adminsters::get(['adminster_email'=>Request::instance()->post('login_email')]);
+				 if($check_login_email){
+				     	 Session::set('jump_msg',['type'=>'warning','msg'=>'邮箱已绑定其他账号~请更换','data'=>'']);
+					 $this->redirect($this->history['0']);
+				 }
 			 }
-			 $check_login_name=Adminsters::get(['adminster_login'=>$r['login_name']]);
+			 $check_login_name=Adminsters::get(['adminster_login'=>$r['adminster_login']]);
 			 if($check_login_name){
 				 Session::set('jump_msg',['type'=>'warning','msg'=>'用户名已存在~请重试','data'=>'']);
 				 $this->redirect($this->history['0']);
 			 }
-			 $adminster->adminster_login=Request::instance()->post('login_name');
+			 $adminster->adminster_login=$r['adminster_login'];
 			 $adminster->adminster_pwd=encryption(Request::instance()->post('login_passwd'),$code);
 			 $adminster->adminster_salt=$code;
 			 $adminster->adminster_update_time=date('Y-m-d H:i:s');
-			 $adminster->adminster_email=Request::instance()->post('login_email');
+			 $adminster->adminster_email=$r['login_email'] ?? '';
 			 $authGroupAccesss=new AuthGroupAccesss;
 			 $authGroupAccesss->group_id=$r['login_group']?$r['login_group']:Config::get('default_groups');
 			 $adminster->profile=$authGroupAccesss;
