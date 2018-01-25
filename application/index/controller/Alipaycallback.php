b3ae059a2b4e7046cc611ce939964f0cc376571d
928791694@qq.com <928791694@qq.com>
2018-01-25 22:30:35 +0800

    'alipaycallback'

--- application/index/controller/Alipaycallback.php
+++ application/index/controller/Alipaycallback.php
@@ -24,9 +24,6 @@ class Alipaycallback
 	 public function callback()
 	 {
          $data=$_POST;
-         // $str = var_export($data,TRUE);
-         // file_put_contents('ceshi.txt',$str);
-        // var_dump($data);die;
          $Alipay=new \app\index\controller\Alipay();
          $success=$Alipay->callback($data);
          if($success!="SUCCESS")
