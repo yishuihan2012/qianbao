3750f66d5cfd4ef08ee2ccfb571ca88bf7e038ed
xuchengcheng <1374687780@qq.com>
2018-02-01 21:51:46 +0800

    代还添加最后更新时间

--- template/Userurl/repayment_plan_detail.blade.php
+++ template/Userurl/repayment_plan_detail.blade.php
@@ -85,7 +85,7 @@
 	 					<div style="padding:0 10px">
 							<p class="invalid-color f14">失败原因：{{$v['back_statusDesc']}}</p>
 							@if(isset($v['is_first']) && $v['is_first']=1)
-							<p class="bor-bot f16 ftr wrap"><a id="resetBtn" plan_id="{{$v['order_id']}}">重新执行</a></p>
+							<p class="bor-bot f16 ftr wrap">最后更新时间：{{$v['order_edit_time']}}<a id="resetBtn" plan_id="{{$v['order_id']}}">重新执行</a></p>
 							@endif
 						</div>
 						@elseif($v['order_status']==2)
