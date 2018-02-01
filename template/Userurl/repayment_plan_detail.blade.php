fdb1770f2941ce49e45eba398f119190fb1ae796
xuchengcheng <1374687780@qq.com>
2018-02-01 22:08:25 +0800

    修改最后执行时间

--- template/Userurl/repayment_plan_detail.blade.php
+++ template/Userurl/repayment_plan_detail.blade.php
@@ -83,8 +83,8 @@
 					        </p>
  					  </div>
 	 					<div style="padding:0 10px">
-							<p class="invalid-color f14">失败原因：{{$v['order_edit_time']}}</p>
-							<p class="invalid-color f14">最后执行时间{{$v['back_statusDesc']}}</p>
+							<p class="invalid-color f14">失败原因：{{$v['back_statusDesc']}}</p>
+							<p class="invalid-color f14">最后执行时间{{$v['order_edit_time']}}</p>
 							@if(isset($v['is_first']) && $v['is_first']=1)
 							<p class="bor-bot f16 ftr wrap"><a id="resetBtn" plan_id="{{$v['order_id']}}">重新执行</a></p>
 							@endif
