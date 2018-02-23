5e3419de7bc30029c0756e4a0618e269bbe1b8ef
xuchengcheng <1374687780@qq.com>
2018-02-23 18:01:06 +0800

    huilian

---
+++ application/api/controller/Huilianjinchuang.php
@@ -0,0 +1,68 @@
+<?php
+ namespace app\api\controller;
+ use think\Db;
+ use app\index\model\Member;
+ use app\index\model\System;
+ use app\index\model\Wallet;
+ use app\index\model\WalletLog;
+ use app\index\model\MemberGroup;
+ use app\index\model\PassagewayItem;
+ use app\index\model\MemberRelation;
+ use app\index\model\MemberCert;
+ use app\index\model\MemberCashcard;
+ use app\index\model\Passageway;
+ use app\index\model\Generation;
+ use app\index\model\GenerationOrder;
+ use app\index\model\Reimbur;
+ use app\index\model\MemberNet as MemberNets;
+ use app\index\model\MemberCreditcard;
+ /**
+ *  @version Huilianjinchuang controller / Api 代还入网
+ *  @author 许成成(1015571416@qq.com)
+ *   @datetime    2018-02-23 15:13:05
+ *   @return 
+ */
+ class Huilianjinchuang{
+ 	/**
+ 	 * 进件请求
+ 	 * @return [type] [description]
+ 	 */
+ 	public function income(){
+
+ 	}
+ 	/**
+ 	 * 下单支付
+ 	 * @return [type] [description]
+ 	 */
+ 	public function pay(){
+
+ 	}
+ 	/**
+ 	 * 代付
+ 	 * @return [type] [description]
+ 	 */
+ 	public function qfpay(){
+
+ 	}
+ 	/**
+ 	 * 订单状态查询
+ 	 * @return [type] [description]
+ 	 */
+ 	public function order_status(){
+
+ 	}
+ 	/**
+ 	 * 余额查询
+ 	 * @return [type] [description]
+ 	 */
+ 	public function query_remain(){
+
+ 	}
+ 	/**
+ 	 * 验签
+ 	 * @return [type] [description]
+ 	 */
+ 	public function get_sign(){
+
+ 	}
+ }
