a93a5d0f663f6c444f474e7679261686c0c4179d
xuchengcheng <1374687780@qq.com>
2018-02-24 09:21:33 +0800

    huilian

--- application/api/controller/Huilianjinchuang.php
+++ application/api/controller/Huilianjinchuang.php
@@ -23,12 +23,46 @@
  *   @return 
  */
  class Huilianjinchuang{
+ 	protected $url;
+ 	public function __construct(){
+ 		$this->url='http://120.77.180.22:8089/v1.0/facade';
+ 		$
+ 	}
  	/**
  	 * 进件请求
  	 * @return [type] [description]
  	 */
  	public function income(){
-
+ 		$agentId=1001001;
+ 		$idcard='370983199109202832';
+ 		$name="许成成";
+ 		$bankId=122;
+ 		$bankCard='6215590200003242971';
+ 		$bankName='招商银行';
+ 		$arr=array(
+ 			'version'=>1.0
+			'charset'=>'UTF-8',//	编码方式UTF-8
+			'agentId'=>$agentId,//受理方预分配的渠道代理商标识
+			'nonceStr'=>make_rand_code(),//随机字符串，字符范围a-zA-Z0-9
+			'signType'=>'RSA',//签名方式，固定RSA
+			'isCompay'=>'0',//对公对私标识0为对私，1为对公
+			'idcardType'=>'01',//证件类型 暂只支持 01 身份证
+			'idcard'=>$idcard,//证件号码
+			'name'=>$name,//姓名
+			'phone'=>$phone,//手机号
+			'bankId'=>$bankId,//联行号
+			'bankCard'=>$bankCard,//银行卡号
+			'bankName'=>$bankName,//开户行名称
+			'bankNo'=>$bankNo,//开户行代码(PAB)
+			'rate'=>$rate//费率‱ ，不小于代理商费率
+			'extraFee'=>$extraFee,//手续费(分)
+			'address'=>'',//N(String)	地址
+			'remark'=>'汇联金创代还',//备注
+ 		);
+ 		$
+ 		$sign=$this->get_string($arr);
+ 		$arr['sign']=$sign,//签名数据
+ 		$url=$this->url.'/report';
  	}
  	/**
  	 * 下单支付
@@ -65,4 +99,36 @@
  	public function get_sign(){
 
  	}
+ 	/**
+ 	 * 获取请求字符串
+ 	 * @param  [type] $arr [description]
+ 	 * @return [type]      [description]
+ 	 */
+ 	public function get_string($arr){
+ 		$arr=$this->SortByASCII($arr);
+ 		$string=http_build_query($arr);
+
+ 	}
+ 	/**
+     * 数组按照ASCII码排序
+     * @return [type] [description]
+     */
+    public function SortByASCII($arr){
+        $keys=array_keys($arr);
+        $newrr=[];
+        foreach ($keys as $k => $v) {
+            if(!$v){
+                exit(json_encode(['code'=>101,'msg'=>'参数'.$k.'获取失败','data'=>'']));
+            }
+            $newrr[$k]['asc']=ord($v);
+            $newrr[$k]['key']=$v;
+            $keys[$k]=ord($v);
+        }
+        array_multisort($keys, SORT_ASC, $newrr);
+        $return=[];
+        foreach ($newrr as $k => $v) {
+           $return[$v['key']]=$arr[$v['key']];
+        }
+        return $return;
+    }
  }
