13b8313d3ea15d7dee76f7b75aa5e166d41d0f1b
yangde1990 <2550607202@qq.com>
2018-02-24 18:52:51 +0800

    功能模块

--- template/Userurl/credit_card.blade.php
+++ template/Userurl/credit_card.blade.php
@@ -3,7 +3,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title></title>
+		<title>{{$info->list_name}}</title>
 		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
 		<link href="/static/css/mui.min.css" rel="stylesheet" />
 		<link href="/static/css/iconfont.css" rel="stylesheet" />
