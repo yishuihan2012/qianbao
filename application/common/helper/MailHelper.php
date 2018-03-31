141dbcbd7a8aa2ce10f976e54e460f114324203c
xuchengcheng <1374687780@qq.com>
2018-03-31 09:53:47 +0800

    错误异常处理

---
+++ application/common/helper/MailHelper.php
@@ -0,0 +1,42 @@
+<?php
+namespace app\common\helper;
+use Nette\Mail\Message;
+use Nette\Mail\SmtpMailer;
+use app\index\model\System;
+class MailHelper
+{
+    /**
+     * 邮件发送：默认系统通知
+     * @param string $title 邮件标题
+     * @param string $content 邮件内容
+     * @param array $email email 支持多个
+     */
+    public static function send($title = '', $content = '', $email = [], $config = [])
+    {
+        $config = $config ? : config('mail.no-reply');
+        $mail = new Message();
+        $mail->setFrom("{$config['showname']} <{$config['username']}>")
+            ->setSubject($title)
+            ->setHtmlBody($content);
+        foreach ($email as $value) {
+            $mail->addTo($value);
+        }
+        $mailer = new SmtpMailer($config);
+        $mailer->send($mail);
+    }
+    /**
+     * 邮件发送：系统异常
+     * @param string $title
+     * @param string $content
+     * @param array $email
+     */
+    public static function errorSend($title = '', $content = '', $email = [])
+    {
+        // $config = config('mail.system-error');
+        $config=Config::load('config');
+        var_dump($config);die;
+        // $config['showname']=System::getName('sitename');
+        $email = $email ? : config('mail.email');
+        self::send($title, $content, $email, $config);
+    }
+}
--- application/config.php
+++ application/config.php
@@ -185,7 +185,7 @@ return [
     // 显示错误信息
     'show_error_msg'         => false,
     // 异常处理handle类 留空使用 \think\exception\Handle
-    'exception_handle'       => '',
+    'exception_handle'       => '\\app\\common\\exception\\Http',
 
     // +----------------------------------------------------------------------
     // | 日志设置
@@ -383,5 +383,28 @@ return [
 
 
         'rsa_private_key'     => "MIIEpAIBAAKCAQEA6zbdTOUzarCq+ISXi3AmLh0PE7RAKV+UjFijRsQaktjUClYw917is4J/y5u/nmKWCTIWqk5ynu2WLVUYWjYe9Cy8f0sOF4eLdqzJcQ4ByepWTvHa7zowwObFiTg4RmHW/jIzfdK/H0Nh1/P/8/kCwwZdtZkWSeEa4MxY1KTNj1PP2trCIdpLWohXHHpITjGYsdpZ4oOLPV81XohOfdjYx1n6Vy1phRz4JK7u1d0Q/e0JUXA86zxpBwOSe/rgB2VReK0MXok91AIKBVfdAxv8s9dZVuHQikT8nu8sMC2NHtqD1EBGH35Gy3kwTVdc69GOZeO7YvFuE0Pv6bCthbGaIwIDAQABAoIBAG7xsfBTK61nJz2RTOm4PEoD6tigbKMXMjoAlQML/HzHSTyW6wHC/6ssIpsnG3rZhWtQ9N/foLDV38ACIzppzK/fagmgnUT76gxP87QfpzhWD9+stFeiWtyh/Qp8Og74ly/O/fqbLQrR/ZSewluRWHwkxI6jZp/yTXs656tlEJBJUHYxrAn+50wSfewIkFg0AFt0+eVcmPvSIVyvjxxrt9Tws8uFvGJ+Z9XTS7YIsvFxq+D1FV29S9+KLXFJyV7i1nR8lU+16GN9PGkyN/7JbGWgSSAz6ohGpluvN9wHrcraVs9j7Tk+TWVpCqjjKmPG9mHFoWEjbnw5uUOKL9FDgAECgYEA+sX2DqKxqRuo8b74nz/jIzpf3ynIN0jkoqAv138v9qcPefLZthiU2Rl0NAp/YE8zXnDb+RUl5xXKQ40N75TuY9pqur6MACrw11Ixr1wHVQijXSRrlZvcNT5xR3fnxCCqw3IBzEK8etataWO/4eBe6IzmZh+3EgxLzF7+7Aes8wECgYEA8B3i090lk1BqLbmtJu3wcRk5HCjo7GYTgezY5at9QP6QZFUCis8oocqbchqzNtOGAzyHn+dsjmyJO+PeJJUUbsAF0LpluGo2kLmnOn9RP+zSEvvk6mZRElsDS/sPHJUs0pbCeq5I5aGpdSQLZUGhmmE86NE70fWs9oM+2JX5YSMCgYAV/ZQVNnkj3jQTdu6wcFbHuQ+9LxnKl07NYASx/gpDs+SsifpDeKZc31mMUrtWw7s6deUkU5FuUT/aap0lPEcnZHjlhV4biahzCVcoDIbsEwTYXyA8D13hIWenn8EoNsEiwHWd5uBGWVr9meyyrw1DkU+G9xMDtyZSdqYeJpIpAQKBgQDN4NIb1gHhzZE8o3dTabA/SOEuw+TyzJLtU6alReSfVaV4YWtMmVK9Znrh2wLOf47oN34SHNt3QphoQXi25Rwf2YUi8q9E78zZSlWYUpHXvbZhkJk2YYQhuOeavii22TEVXy5hAg5+1Z32UnFy4FMvizAubzhbTcp/kwjPq8enAQKBgQDu+/PJH275HPMijaS8Tnwa5+6Qqr0DzjquPfrrJXJQjDJfVg8bamDPN5RcSbVpBTzXeh7raKAkyI22g51fZgZaK2Un9Cr0Y3uldBB5Vbr7RHdwlDvICCw3EmOoZ7hzMPrhs8bG0aSGw+QAIOSMR9s587axpO564YPviEB9TwNWzQ=="
-    ]
+    ],
+    'mail' => [ //邮件
+        'no-reply' => [
+            'showname' => '钱包',
+            'host' => 'smtp.qq.com',
+            'username' => '1374687780@qq.com',
+            'password' => 'smiaxcmxvgljfiae',
+            'secure' => 'ssl'
+        ],
+        'system-error' => [
+            'showname' => '网站异常',
+            'host' => 'smtp.qq.com',
+            'username' => '1374687780@qq.com',
+            'password' => 'smiaxcmxvgljfiae',
+            'secure' => 'ssl'
+        ],
+        'email' => [    // 异常发送对象
+            // '1015571416@qq.com',
+            '1374687780@qq.com',
+            // 'lwtlove1314@qq.com',
+            '794633291@qq.com',//star
+            '3115317085@qq.com',//yang
+        ]
+    ],
 ];
