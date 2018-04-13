62144f2438fb1226e49aea7539fca09254afb414
xuchengcheng <1374687780@qq.com>
2018-04-13 16:56:41 +0800

    daihuan

--- CashOut.php
+++ CashOut.php
@@ -125,6 +125,7 @@ class CashOut
 	      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	      $res = curl_exec($ch);
 	      $result = json_decode($res, true);
+	      return ['code'=>101,'msg'=>json_encode($result)];
 	      // var_dump($result);die;
 	      if ($result['code'] == 0) {
 	      	 $datas=AESdecrypt($result['payload'],$this->passway_info->passageway_pwd_key);
--- application/api/controller/Huiliandaihuan.php
+++ application/api/controller/Huiliandaihuan.php
@@ -20,12 +20,10 @@
  use app\index\model\MemberCreditPas;
  class Huiliandaihuan{
  	protected $url;
- 	protected $agentId;
  	protected $version;
  	public function __construct(){
  		$this->version='1.0';
- 		$this->url='http://120.77.180.22:8089/v1.0/facade';
- 		$this->agentId='1001003';
+ 		$this->url='http://39.108.137.8:8099/v1.0/facade';
  	}
  	/**
  	 * 进件
@@ -60,7 +58,7 @@
  			'version'=>$this->version,
             'charset'=>'UTF-8',//   编码方式UTF-8
             'agentId'=>$agentId,//受理方预分配的渠道代理商标识
-            'nonceStr'=>make_rand_code(),//随机字符串，字符范围a-zA-Z0-9
+            'nonceStr'=>generate_password(16),//随机字符串，字符范围a-zA-Z0-9
             'signType'=>'RSA',//签名方式，固定RSA
             'isCompay'=>'0',//对公对私标识0为对私，1为对公
             'idcardType'=>'01',//证件类型 暂只支持 01 身份证
@@ -89,38 +87,117 @@
  	 *  信用卡签约
  	 * @return [type] [description]
  	 */
- 	public function card_sign(){
+ 	public function card_sign($card_id=63,$merId='9000105494',$Passageway=29){
+ 		 //获取行用卡信息
+        $card_info=MemberCreditcard::where(['card_id'=>$card_id])->find();
+        if(!$card_info){
+            return ['code'=>'101','msg'=>'获取信用卡信息失败'];
+        }
+        //获取通道信息
+        $Passageways=Passageway::where(['passageway_id'=>$Passageway])->find();
+        $agentId=$Passageways->passageway_mech;
  		$data=array(
  			'version'=>'1.0',//	版本号 tr (8)	是	目前版本号：1.0
 			'serviceUri'=>"YX0001",	//交易代码	str (8)	是	YX0001
 			'charset'=>'UTF-8',// 编码格式	str (8)	是	
 			'signType'=>"RSA",//签名方式是	
-			'nonceStr'=>'',// 随机字符串		str (32)	是	随机字符串
-			'agentId'=>'', //代理商号		是	受理方预分配的渠道代理商标识
-			'merId'=>'',// 商户号是	进件返回的merId
-			'orderNo'=>'',// 订单号	是	商户交易订单号
-			'phone'=>'',//手机号码	是	银行预留手机号
-			'cardNo'=>'',//银行卡号		是	用于支付的银行卡号(只支持借记卡)
-			'expDate'=>'',//有效期		str (5)	是	信用卡有效期，格式mm-YY
-			'CVN2'=>'',//CVN2	str (3)	是	信用卡安全码后三位
-			'sign'=>'',//签名		str (256)	是	签名字段
+			'nonceStr'=>generate_password(16),// 随机字符串		str (32)	是	随机字符串
+			'agentId'=>$agentId, //代理商号		是	受理方预分配的渠道代理商标识
+			'merId'=>$merId,// 商户号是	进件返回的merId
+			'orderNo'=>generate_password(16),// 订单号	是	商户交易订单号
+			'phone'=>$card_info->card_phone,//手机号码	是	银行预留手机号
+			'bankCard'=>$card_info->card_bankno,//银行卡号是	用于支付的银行卡号(只支持借记卡)
+			'expDate'=>substr($card_info['card_expireDate'],0,2).'-'.substr($card_info['card_expireDate'],2,2),//N(String)   信用卡时必填，格式:mm-YY
+            'CVN2'=>$card_info['card_Ident'] ,//N(String)   信用卡时必填
  		);
- 		$url=$this->url.'/report';
+ 		echo json_encode($data);
+ 		$url=$this->url.'/repay';
  		$res=$this->request($url,$data);
+ 		var_dump($res);die;
+ 	}
+ 	/**
+ 	 * 確認簽約
+ 	 * @param  string $agentId  [description]
+ 	 * @param  string $merId    [description]
+ 	 * @param  string $orderNo  [description]
+ 	 * @param  string $authCode [description]
+ 	 * @return [type]           [description]
+ 	 */
+ 	public function card_sign_confirm($agentId='1001057',$merId='9000105494',$orderNo='mClfuC2yGOkk0by8',$authCode='850138'){
+ 		$data=array(
+ 			'version'=>'1.0',//版本号	str (8)	是	目前版本号：1.0
+			'serviceUri'=>'YX0002',//交易代码		str (8)	是	
+			'charset'=>'UTF-8',//编码格式 str (8)	是	
+			'signType'=>'RSA',//签名方式str (8) 	是	
+			'nonceStr'=>generate_password(16),//随机字符串	是	随机字符串
+			'agentId'=>$agentId,//代理商号		str (8)	是	受理方预分配的渠道代理商标识
+			'merId'=>$merId,//商户号		str (10)	是	进件返回的merId
+			'orderNo'=>$orderNo,//订单号		str (32)	是	交易订单号(与签约订单号一致)
+			'authCode'=>$authCode,//手机验证码		str (6)	是	签约验证码
+ 		);
+ 		echo json_encode($data);
+ 		$url=$this->url.'/repay';
+ 		$res=$this->request($url,$data);
+ 		var_dump($res);die;
  	}
  	/**
  	 * 上传资料文件
  	 * @return [type] [description]
  	 */
- 	public function upload_material(){
-
+ 	public function upload_material($agentId='1001057',$merId='9000105494',$uid=42){
+ 		$cert=MemberCert::where(['cert_member_id'=>$uid])->find();
+ 		if(!$cert || !$cert->IdPositiveImgUrl || !$cert->IdNegativeImgUrl || !$cert->IdPortraitImgUrl){
+ 			return ['code'=>'101','msg'=>'实名认证信息不全，请补全实名信息。'];
+ 		}
+ 		$data=array(
+ 			'version'=>'1.0',//版本号		str (8)	是	目前版本号：1.0
+			'serviceUri'=>'YX0003',//交易代码		str (8)	是	YX0003
+			'charset'=>'UTF-8',//编码格式	charset	str (8)	是	UTF-8
+			'signType'=>'RSA',//签名方式	signType	str (8) 	是	RSA
+			'nonceStr'=>generate_password(16),//随机字符串		str (32)	是	随机字符串
+			'agentId'=>$agentId,//代理商号		str (8)	是	受理方预分配的渠道代理商标识
+			'merId'=>$merId,//商户号	merId	str (10)	是	进件返回的merId
+			'image1'=>base64_encode(file_get_contents($cert->IdPositiveImgUrl)),//图片字符串		str (256)	是	身份证正面
+			'image2'=>base64_encode(file_get_contents($cert->IdNegativeImgUrl)),//图片字符串		str (256)	是	身份证反面
+			'image3'=>base64_encode(file_get_contents($cert->IdPortraitImgUrl)),//图片字符串		str (256)	是	手持身份证
+ 		);
+ 		echo json_encode($data);
+ 		$url=$this->url.'/repay';
+ 		$res=$this->request($url,$data);
+ 		var_dump($res);die;
  	}
  	/**
  	 * 订单支付
  	 * @return [type] [description]
  	 */
- 	public function order_pay(){
-
+ 	public function order_pay($agentId='1001057',$merId='9000105494'){
+ 		$data=array(
+ 			'version'=>'1.0',//版本号		str (8)	是	目前版本号：
+			'serviceUri'=>'YX0004',//交易代码		str (8)	是	
+			'charset'=>'UTF-8',//编码格式		str (8)	是	
+			'signType'=>'RSA',//签名方式		str (8) 	是	
+			'nonceStr'=>generate_password(16),//随机字符串		str (32)	是	随机字符串
+			'agentId'=>$agentId,//代理商号		str (8)	是	受理方预分配的渠道代理商标识
+			'merId'=>$merId,//商户号		str (10)	是	进件返回的merId
+			'orderNo'=>generate_password(16),//订单号		str (32)	是	交易订单号
+			'bankCard'=>'6258101661675746',//银行卡号		str (16)	是	用于交易的银行卡号
+			'notifyUrl'=>System::getName('system_url').'/Api/Huiliandaihuan/payCallback',//通知地址		str (256)	是	异步通知地址(暂无)
+			'amount'=>'19800',//交易金额		str (8)	是	以分为单位
+ 		);
+ 		echo json_encode($data);
+ 		$url=$this->url.'/repay';
+ 		$res=$this->request($url,$data);
+ 		var_dump($res);die;
+ 	}
+ 	/**
+ 	 * 支付回调
+ 	 * @return [type] [description]
+ 	 */
+ 	public function payCallback(){
+ 		$data = file_get_contents("php://input");
+ 		file_put_contents('hulian_luodi.txt', json_encode($data));
+ 		$data=array(
+ 		);
  	}
  	/**
  	 * 订单查询
@@ -155,7 +232,7 @@
      */
     function pri_encode($data){
         $encrypted='';
-        $private_key=file_get_contents("./static/rsakey/huilian/hldh.pem"); //秘钥
+        $private_key=file_get_contents("./static/rsakey/huilian/1001057_prv.pem"); //秘钥
         $pi_key =  openssl_pkey_get_private($private_key);  //这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id  
         $str='';
         foreach (str_split($data, 117) as $chunk) {
@@ -196,9 +273,10 @@
     public function request($url,$arr){
         $sign=$this->get_sign($arr);
         $arr['sign']=$sign;//签名数据
-        echo json_encode($arr);die;
+        // echo json_encode($arr);die;
         $arr=http_build_query($arr);
         // $arr=urldecode($arr);
+        // echo $arr;die;
         $return=curl_post($url,'post',$arr,0);
         echo $return;die;
         $result=json_decode($return,true);
