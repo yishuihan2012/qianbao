e8175ac52fadb1a08dc612608224e49c9acef3d0
star <star@qq.com>
2018-04-26 11:04:49 +0800

    通道-交易详情 变更到带参对应订单列表

--- template/admin/passageway/index.blade.php
+++ template/admin/passageway/index.blade.php
@@ -50,7 +50,7 @@
                                    <li><a  href="{{url('/index/passageway/list_credit_card','id='.$list['passageway_id'])}}" >查看信用卡列表</a></li>
                                       <li><a  data-remote="{{url('/index/passageway/cashout','id='.$list['passageway_id'])}}" data-toggle="modal" data-size="lg" href="#">通道设置</a></li>
                                       <!-- <li><a  data-remote="{{url('/index/passageway/also','id='.$list['passageway_id'])}}" data-toggle="modal" data-size="lg" href="#">代还设置</a> </li> -->
-                                      <li><a href="{{url('/index/passageway/passageway_details','id='.$list['passageway_id'])}}">交易详情</a> </li>
+                                      <li><a href="/index/{{$list['passageway_also']==1 ? 'order/cash' : 'plan/detail'}}?passageway_id={{$list['passageway_id']}}">交易详情</a> </li>
                                       <li><a href="{{url('/index/passageway/passageway_statistics','passageway_id='.$list['passageway_id'])}}">通道利润统计</a> </li>
                                  </ul>
                            </div>
