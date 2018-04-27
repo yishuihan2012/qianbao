59741a61ded51fe6ad4e27e9ce57affc9c5ad0bc
star <star@qq.com>
2018-04-27 09:35:09 +0800

    代还订单统计 还款总金额应减去还款的手续费

--- application/index/controller/Plan.php
+++ application/index/controller/Plan.php
@@ -251,7 +251,7 @@ class Plan extends Common{
                     if($v['order_type']==1){
 	                    $count['order_cash_money']+=$v['order_money'];
                     }else{
-	                    $count['order_repay_money']+=$v['order_money'];
+		                $count['order_repay_money']+=$v['order_money']-$v['order_pound'];
                     }
                 }
             }
@@ -263,13 +263,13 @@ class Plan extends Common{
             $count['order_platform_fee']=array_sum(array_column($order_data,'order_platform_fee'));
             $cms=db('commission')->where('commission_from','in',array_column($order_data, 'order_id'))->where('commission_type',3)->group('commission_from')->column("commission_from,sum(commission_money) as sum");
             #指定成功状态时
-            $count['order_pound']=array_sum(array_column($order_data,'order_pound'))+array_sum(array_column($order_data,'user_fix'));
+            $count['order_pound']=array_sum(array_column($order_data,'order_pound'));
             $count['sanji']=array_sum($cms);
             foreach ($order_data as $k => $v) {
 	            if($v['order_type']==1){
 	                $count['order_cash_money']+=$v['order_money'];
 	            }else{
-	                $count['order_repay_money']+=$v['order_money'];
+	                $count['order_repay_money']+=$v['order_money']-$v['order_pound'];
 	            }
             }
         }
