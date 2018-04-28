0261158b6a85ddd4df89b760e0bef1cc454dbfb5
xuchengcheng <1374687780@qq.com>
2018-04-28 15:37:52 +0800

    -

--- application/common.php
+++ application/common.php
@@ -228,7 +228,7 @@ function encryption($str, $salt, $method='md5')
                                 'msg'=>'当前数据认证有误，请核实信息后或过30分钟后再试',
                                 'data'=>[
                                     'resultCode'=>'R002',
-                                    'remark'=>'因你填写的资料未作修改，请修改后再次提交'
+                                    'remark'=>'因你填写的资料未作修改，请修改后再次提交,或稍后再试。'
                                 ],
                            ];
             }
@@ -239,7 +239,7 @@ function encryption($str, $salt, $method='md5')
     $post['personName']=$data['name'];
     $post['personPhone']=$data['mobileNo'];
     $CertDatas[]=$post;
-    Cache::set('CertDatas',$CertDatas,1800);
+    Cache::set('CertDatas',$CertDatas,60);
     $headers = array();
     array_push($headers, "Authorization:APPCODE " . System::getName('appcode'));
     // array_push($headers, "Authorization:APPCODE " .'d04d00f17ddd430abc630269b4c30324');
@@ -273,7 +273,7 @@ function BankCert_Java($bankCard='',$personCard='',$personName='',$personPhone='
     if($CertDatas){
         foreach ($CertDatas as $k => $CertData) {
             if($CertData['bankCard']==$bankCard && $CertData['personCard']==$personCard && $CertData['personPhone']==$personPhone && $CertData['personName']==$personName){
-                    return ['code'=>'-1','info'=>'因你填写的资料未作修改，请修改后再次提交'];
+                    return ['code'=>'-1','info'=>'因你填写的资料未作修改，请修改后再次提交,或稍后再试'];
             }
         }
     }
@@ -282,7 +282,7 @@ function BankCert_Java($bankCard='',$personCard='',$personName='',$personPhone='
     $data['personName']=$personName;
     $data['personPhone']=$personPhone;
     $CertDatas[]=$data;
-    Cache::set('CertDatas',$CertDatas,1800);
+    Cache::set('CertDatas',$CertDatas,60);
 
     $url='http://api.xijiakei.com/bankElements/detailApi';
     // $apiAppId='20180329164226439';
