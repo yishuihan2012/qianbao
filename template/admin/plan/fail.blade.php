eb49809136e08d12029226edea56be23f4be513b
star <star@qq.com>
2018-05-05 17:36:06 +0800

    挂账列表

--- template/admin/plan/fail.blade.php
+++ template/admin/plan/fail.blade.php
@@ -1,6 +1,6 @@
  <!--dialog Title-->
 @extends('admin/layout/layout_main')
-@section('title','订单列表管理~')
+@section('title','挂账列表管理~')
 @section('wrapper')
 <style>
    h4 > a,.pull-right > a{color:#145ccd;}
@@ -10,34 +10,14 @@
 <hr/>
 <div class="list">
   <header>
-    <h3><i class="icon-list-ul"></i> 计划列表详情 <small>共 <strong class="text-danger">{{count($list)}}</strong> 条</small></h3>
+    <h3><i class="icon-list-ul"></i> 挂账列表详情 <small>共 <strong class="text-danger">{{count($list)}}</strong> 条</small></h3>
   </header>
    <div class="panel">
     <div class="panel-body">
       <form action="" name="myform" class="form-group" method="get">
-          <div class="input-group" style="width: 150px;float: left;margin-right: 20px;">
-            <span class="input-group-addon">会员名称</span>
-            <input type="text" class="form-control" name="member_nick" value="{{$r['member_nick']}}" placeholder="会员名称" >
-          </div>
-
-          <div class="input-group" style="width: 200px;float: left;margin-right: 20px;">
-            <span class="input-group-addon">手机号</span>
-            <input type="text" class="form-control" name="member_mobile" value="{{$r['member_mobile']}}" placeholder="手机号">
-          </div>
-          <div class="input-group" style="width: 200px;float: left;margin-right: 20px;">
-            <span class="input-group-addon">订单金额</span>
-            <input type="text" class="form-control" name="order_money" value="{{$r['order_money']}}" placeholder="订单金额">
-          </div> 
-          <div class="input-group" style="width: 200px;float: left;margin-right: 20px;">
-            <span class="input-group-addon">计划ID</span>
-            <input type="text" class="form-control" name="order_no" value="{{$r['order_no']}}" placeholder="计划ID">
-          </div> 
-          <div class="input-group" style="width: 200px;float: left; margin-right: 10px;">
-              <input type="text" class="form-control date-picker" id="dateTimeRange" placeholder="执行时间" />
-              <input type="hidden" name="beginTime" id="beginTime" value="" />
-              <input type="hidden" name="endTime" id="endTime" value="" />
-              <z class='clearTime'>X</z>
-          </div>
+      <div class="input-group" style="width: 360px;float: left;margin-right: 10px;">
+        <span class="input-group-addon">日期</span>
+        <input type="date" name="beginTime" style="width: 140px" class="form-control" value="{{$r['beginTime'] or ''}}" />
           <button class="btn btn-primary" type="submit">搜索</button>
     </form>
     </div>
@@ -51,68 +31,37 @@
            <thead>
             <tr>
               <th>计划ID</th>
-              <th>执行记录ID</th>
               <th>通道</th>
               <th>会员名称</th>
-              <th>订单消费类型</th>
               <th>信用卡号</th>
-              <th>订单金额</th>
-              <th>扣除手续费后金额</th>
-              <th>订单手续费</th>
-              <th>订单状态</th>
-              <th>订单描述</th>
-              <th>订单执行时间</th>
-              <th>订单更新时间</th>
-              <th>订单创建时间</th>
-              <th>失败原因</th>
-              <th>操作</th>
+              <th>挂账金额</th>
           </tr>
       </thead>
      <tbody>
     @foreach($list as $key => $value)
      <tr style="">
-       <td>{{$value->order_no}}</td>
-       <td>{{$value->order_id}}</td>
-       <td>{{$value->passageway_name}}</td>
-       <td>{{$value->member_nick}}</td>
-       <td>@if($value->order_type == 1) <em style="color:#00FF00;"> 消费</em> @else <em style="color:#00FFFF;">还款</em>@endif </td>
-       <td>{{$value->order_card}}</td>
-       <td>{{$value->order_money}}</td>
-       <td>{{$value->order_real_get}}</td>
-       <td>{{$value->order_pound}}</td>
-       <td>@if($value->order_status == 1)<em style="color:#FF9900;">  待执行 </em>@elseif($value->order_status == 2)<em style="color:#33FF33;"> 成功</em> @elseif($value->order_status == 3)<em style="color:#FF00FF;"> 取消</em> @elseif($value->order_status ==4) <em style="color:#00FFFF;">带查证<@elseif($value->order_status == 5)<em style="color:#FF00FF;"> 已处理</em> /em> @else <em style="color:red;">失败 </em>@endif </td>
-       <td>{{$value->order_desc}}</td>
-       <td>{{$value->order_time}}</td>
-       <td>{{$value->order_edit_time}}</td>
-       <td>{{$value->order_add_time}}</td>
-       <td>{{$value->back_statusDesc}}</td>
+       <td>{{$value['order_no']}}</td>
+       <td>{{$value['passageway_name']}}</td>
+       <td>{{$value['member_nick']}}</td>
+       <td>{{$value['order_card']}}</td>
        <td>
-        @if($value->order_status == 3)
-          @endif
-          @if($value->order_status == 1)
-          <a class="remove" href="#" data-url="{{url('/api/Memnernet/action_single_plan/id/'.$value['order_id'])}}"><i class="icon-pencil"></i> 立即执行 </a>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <a class="remove" href="#" data-url="{{url('/index/Plan/order_status/status/3/id/'.$value['order_id'])}}"><i class="icon-pencil"></i> 取消执行 </a>
-          @endif
-          @if($value->order_status == -1)
-          <a class="remove" href="#" data-url="{{url('/api/Membernet/action_single_plan/id/'.$value['order_id'])}}"><i class="icon-pencil"></i> 重新执行 </a>
-          @endif
-          <a class="btn btn-sm"  href="{{url('/index/Plan/info/id/'.$value['order_no'])}}" >查看详情</a>
-       </td>
+        <a href="/index/plan/detail?order_id={{$value['order_id']}}">
+        {{$value['sums']}}
+      </a>
+      </td>
      </tr>
      @endforeach
       </tbody>
   </table>
 
   </div>
-  {!!$list->render()!!}
 </div>
 </section>
 <script>
  
   $(document).ready(function(){
        $('.menu .nav .active').removeClass('active');
-       $('.menu .nav li.plan_fail').addClass('active');
+       $('.menu .nav li.plan_fails').addClass('active');
        $('.menu .nav li.plan-manager').addClass('show');
 
        $(".parent li a").click(function(){
@@ -156,88 +105,8 @@
         }
      });
        })
-  @if(isset($r["beginTime"]))
-      console.log(666);
-  //初始化时间
-      $('#dateTimeRange').val('{{$r["beginTime"]}} - {{$r["endTime"]}}');
-      $('#beginTime').val('{{$r["beginTime"]}}');
-      $('#endTime').val('{{$r["endTime"]}}'); 
-  @endif
-});
-  $('#dateTimeRange').daterangepicker({
-        applyClass : 'btn-sm btn-success',
-        cancelClass : 'btn-sm btn-default',
-        locale: {
-            applyLabel: '确认',
-            cancelLabel: '取消',
-            fromLabel : '起始时间',
-            toLabel : '结束时间',
-            customRangeLabel : '自定义',
-            firstDay : 1
-        },
-        ranges : {
-            //'最近1小时': [moment().subtract('hours',1), moment()],
-            '今日': [moment().startOf('day'), moment()],
-            '昨日': [moment().subtract('days', 1).startOf('day'), moment().subtract('days', 1).endOf('day')],
-            '最近7日': [moment().subtract('days', 6), moment()],
-            '最近30日': [moment().subtract('days', 29), moment()],
-            '本月': [moment().startOf("month"),moment().endOf("month")],
-            '上个月': [moment().subtract(1,"month").startOf("month"),moment().subtract(1,"month").endOf("month")]
-        },
-        opens : 'left',    // 日期选择框的弹出位置
-        separator : ' 至 ',
-        showWeekNumbers : true,     // 是否显示第几周
-
- 
-        //timePicker: true,
-        //timePickerIncrement : 10, // 时间的增量，单位为分钟
-        //timePicker12Hour : false, // 是否使用12小时制来显示时间
- 
-         
-        //maxDate : moment(),           // 最大时间
-        format: 'YYYY-MM-DD'
- 
-    }, function(start, end, label) { // 格式化日期显示框
-        $('#beginTime').val(start.format('YYYY-MM-DD'));
-        $('#endTime').val(end.format('YYYY-MM-DD'));
-    });
-// begin_end_time_clear();
-$('.clearTime').click(begin_end_time_clear);
-  //清除时间
-    function begin_end_time_clear() {
-        $('#dateTimeRange').val('');
-        $('#beginTime').val('');
-        $('#endTime').val('');
-    }
-    function getNowFormatDate() {
-        var date = new Date();
-        var seperator1 = "-";
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1;
-        var strDate = date.getDate();
-        if (month >= 1 && month <= 9) {
-            month = "0" + month;
-        }
-        if (strDate >= 0 && strDate <= 9) {
-            strDate = "0" + strDate;
-        }
-        var currentdate = year + seperator1 + month + seperator1 + strDate;
-        
-        return currentdate;
-    }
-     $('#beginTime').val(getNowFormatDate());
-     $('#endTime').val(getNowFormatDate());
+})
 </script>
 <style type="text/css">
-   .clearTime{
-    position: absolute;
-    right: 5px;
-    top: 5px;
-    z-index: 99;
-    border: 1px solid;
-    color: red;
-    font-size: .6rem;
-    padding: 0 5px;
-   }
  </style>
 @endsection
