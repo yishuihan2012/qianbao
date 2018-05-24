ddfd7ff810b79362490f78d1197b112dea897f8b
xuchengcheng <1374687780@qq.com>
2018-05-24 15:53:21 +0800

    -

--- application/index/controller/CashOut.php
+++ application/index/controller/CashOut.php
@@ -1077,9 +1077,13 @@ class CashOut
 	 */
 	public function yilianpay($tradeNo,$price,$description='快捷落地支付'){
 		$Yilian=new \app\api\payment\Yilian();
-		$res=$Yilian->pay($this->member_infos,$this->member_cert,$this->member_card,$this->card_info, $this->also,$price);
-		if($res){
-			$order_result=$this->writeorder($tradeNo, $price, $price*($this->also->item_rate/100) ,$description,$tradeNo);
+		$res=$Yilian->pay($this->member_infos,$this->member_cert,$this->member_card,$this->card_info, $this->also,$price,$tradeNo);
+		if($res &&$res['respCode']=='0000'){
+			$order_result=$this->writeorder($tradeNo, $price, $price*($this->also->item_rate/100),$description,$tradeNo);
+			$url=System::getName('system_url').'/api/Userurl/nohtml/data/'.base64_encode($res['pay_info']);
+			return ['code'=>'200','msg'=>'下单成功','data'=>['type'=>2,'url'=>$url]];
+		}else{
+			return ['code'=>'102','msg'=>$res['respMsg']];
 		}
 	}
 	 /**
