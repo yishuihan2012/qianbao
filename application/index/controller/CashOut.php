6f9624defe6abb92cb66760532511d3961af3cc2
xuchengcheng <1374687780@qq.com>
2018-05-29 14:21:37 +0800

    -

--- application/index/controller/CashOut.php
+++ application/index/controller/CashOut.php
@@ -1114,7 +1114,7 @@ class CashOut
         $rand=rand(0,count($passageway_list)-1);
         $channelId=$passageway_list[$rand]['channelId'];
         #4支付
-		$res=$YiJiFu->pay($this->member_infos,$this->member_cert,$this->member_card,$this->card_info,$this->also,$price,$tradeNo,$channelId);
+		$res=$YiJiFu->pay($this->member_infos,$this->member_cert,$this->member_card,$this->card_info,$this->also,$price,$tradeNo,$channelId,$material_id);
 		$url_data=base64_encode($res['url']);
 		// $order_result=$this->writeorder($tradeNo, $price, $price*($this->also->item_rate/100),$description,$tradeNo);
 		$url=System::getName('system_url').'/api/Userurl/headerurl/data/'.$url_data;
