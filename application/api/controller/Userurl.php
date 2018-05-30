829ca11d8accd141a2128313e21c81b8e5e3f9b6
xuchengcheng <1374687780@qq.com>
2018-05-30 15:40:21 +0800

    -

--- application/api/controller/Userurl.php
+++ application/api/controller/Userurl.php
@@ -1787,7 +1787,7 @@ class Userurl extends Controller
     public function jiyifuturnback(){
         $params=input();
         file_put_contents('turnback.txt', json_encode($params));
-        $message=isset($params['resultMessage'])?$params['resultMessage']:$params['message'];
+        $message=isset($params['message'])?$params['message']:$params['resultMessage'];
         $this->assign('data',$message);
         return view("Userurl/show_error");
         
