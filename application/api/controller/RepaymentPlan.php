4cbf76137ce899a627e4e7d470b0b3ce134a0460
xuchengcheng <1374687780@qq.com>
2018-05-31 17:01:38 +0800

    -

--- application/api/controller/RepaymentPlan.php
+++ application/api/controller/RepaymentPlan.php
@@ -123,8 +123,7 @@
                         // $count=ceil($this->param['billMoney']*1.44/$single_max);
                         $gongshi=$this->param['billMoney']*1.44/$single_max/$days;
                         $zheng=intval(floor($gongshi));
-                        $yu=ceil($gongshi-$zheng);
-                        $count_min=($zheng+$yu)*$days;
+                        $count_min=($zheng*$days+ceil(($gongshi-$zheng)*$days));
                         return['code'=>'101','msg'=>'该行单次消费限额'.$single_max."元，请设置刷卡次数最少为{$count_min}次。"];
                         // return['code'=>'101','msg'=>'该行单次消费限额'.$single_max."元，请确保还款次数不低于{$count}次。"];
                     }
